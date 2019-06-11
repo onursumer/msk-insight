02ccaab5598ee53581261ffe96c91b919d55a821
Onur Sumer <s.onur.sumer@gmail.com>
2019-06-11 17:55:27 -0400

    added mutation mapper into gene page

--- client/src/components/MutationMapper.tsx
+++ client/src/components/MutationMapper.tsx
@@ -1,61 +1,35 @@
-import {action, observable} from "mobx";
 import {observer} from "mobx-react";
 import * as React from "react";
 
-import {DefaultMutationMapperStore, LollipopMutationPlot} from "react-mutation-mapper";
-
-import {IMutation} from "../../../server/src/model/Mutation";
-import {DataStatus} from "../store/DataStatus";
+import {
+    DefaultMutationMapperStore,
+    LollipopMutationPlot,
+    Mutation
+} from "react-mutation-mapper";
 
 interface IMutationMapperProps
 {
-    dataPromise: Promise<IMutation[]>;
+    data: Mutation[];
     hugoSymbol: string;
 }
 
 @observer
 class MutationMapper extends React.Component<IMutationMapperProps>
 {
-    @observable
-    private data: IMutation[] = [];
-
-    @observable
-    private status: DataStatus = 'pending';
-
     public render()
     {
-        return this.status === 'pending' ? (
-            <i className="fa fa-spinner fa-pulse fa-2x" />
-        ): (
-            // TODO fetchVariantAnnotationsIndexedByGenomicLocation(this.rawMutations, ["annotation_summary","hotspots"], AppConfig.serverConfig.isoformOverrideSource)
+        return (
             <LollipopMutationPlot
                 store={
                     new DefaultMutationMapperStore(
+                        // TODO entrezGeneId?
                         {hugoGeneSymbol: this.props.hugoSymbol} as any,
                         {isoformOverrideSource: "mskcc"},
-                        () => this.data as any)
+                        () => this.props.data)
                 }
                 geneWidth={666}
             />
         );
-    }
-
-    public componentDidMount()
-    {
-        this.props.dataPromise
-            .then(this.handleDataLoad)
-            .catch(this.handleDataError);
-    }
-
-    @action.bound
-    private handleDataLoad(mutations: IMutation[]) {
-        this.data = mutations;
-        this.status = 'complete';
-    }
-
-    @action.bound
-    private handleDataError(reason: any) {
-        this.status = 'error';
     }
 }
 
--- client/src/pages/Gene.tsx
+++ client/src/pages/Gene.tsx
@@ -1,8 +1,16 @@
-import {computed} from "mobx";
+import {action, computed, observable} from "mobx";
 import {observer} from "mobx-react";
 import * as React from 'react';
 
+import {
+    annotateMutations,
+    fetchVariantAnnotationsIndexedByGenomicLocation,
+    Mutation, resolveDefaultsForMissingValues
+} from 'react-mutation-mapper';
+
+import {IMutation} from "../../../server/src/model/Mutation";
 import MutationMapper from "../components/MutationMapper";
+import {DataStatus} from "../store/DataStatus";
 import {fetchMutationsByGene} from "../util/MutationDataUtils";
 
 interface IGeneProps
@@ -17,6 +25,18 @@ interface IGeneProps
 @observer
 class Gene extends React.Component<IGeneProps>
 {
+    @observable
+    private annotatedMutations: Mutation[] = [];
+
+    @observable
+    private insightMutations: IMutation[] = [];
+
+    @observable
+    private insightStatus: DataStatus = 'pending';
+
+    @observable
+    private annotationStatus: DataStatus = 'pending';
+
     @computed
     private get hugoSymbol() {
         return this.props.match.params.hugoSymbol;
@@ -24,14 +44,55 @@ class Gene extends React.Component<IGeneProps>
 
     public render()
     {
-        return (
+        return this.insightStatus === 'pending' || this.annotationStatus === 'pending' ? (
+            <i className="fa fa-spinner fa-pulse fa-2x" />
+        ): (
             <div>
                 <MutationMapper
                     hugoSymbol={this.hugoSymbol}
-                    dataPromise={fetchMutationsByGene(this.hugoSymbol)}
+                    data={this.annotatedMutations}
                 />
             </div>
         );
+    }
+
+    public componentDidMount()
+    {
+        fetchMutationsByGene(this.hugoSymbol)
+            .then(this.handleInsightDataLoad)
+            .catch(this.handleInsightDataError);
+    }
+
+    @action.bound
+    private handleInsightDataLoad(mutations: IMutation[])
+    {
+        this.insightStatus = 'complete';
+        this.insightMutations = mutations;
+
+        fetchVariantAnnotationsIndexedByGenomicLocation(mutations, ["annotation_summary", "hotspots"], "mskcc")
+            .then(this.handleVariantAnnotationDataLoad)
+            .catch(this.handleVariantAnnotationDataError)
+    }
+
+    @action.bound
+    private handleInsightDataError(reason: any) {
+        this.insightStatus = 'error';
+    }
+
+    @action.bound
+    private handleVariantAnnotationDataLoad(indexedVariantAnnotations: {[genomicLocation: string]: any})
+    {
+        this.annotationStatus = 'complete';
+
+        const mutations = annotateMutations(this.insightMutations, indexedVariantAnnotations);
+        resolveDefaultsForMissingValues(mutations);
+
+        this.annotatedMutations = mutations as Mutation[];
+    }
+
+    @action.bound
+    private handleVariantAnnotationDataError(reason: any) {
+        this.annotationStatus = 'error';
     }
 }
 
